c79b0d13df4c925123be94f4310445b43fa7cf32
ANDRIAN-HOME-WORKSTATION <andrian.hubencu@codwer.com>
2021-11-29 23:18:28 +0200

    my custom thread

--- lab1/lab1/lab1/Program.cs
+++ lab1/lab1/lab1/Program.cs
@@ -2,11 +2,28 @@
 
 namespace lab1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var t1 = new MyThread("Thread 1", GetRandomNumber(3000));
+            var t2 = new MyThread("Thread 2", GetRandomNumber(3000));
+            var t3 = new MyThread("Thread 3", GetRandomNumber(3000));
+            var t4 = new MyThread("Thread 4", GetRandomNumber(3000));
+            var t5 = new MyThread("Thread 5", GetRandomNumber(3000));
+            var t6 = new MyThread("Thread 6", GetRandomNumber(3000));
+
+            t2.WaitFor(t1);
+            t3.WaitFor(t1, t2);
+            t4.WaitFor(t2, t3);
+            t5.WaitFor(t4);
+            t6.WaitFor(t4, t5);
+
+            MyThread[] threads = { t1, t2, t3, t4, t5, t6 };
+
+            foreach (var thread in threads) thread.Start();
         }
+
+        private static int GetRandomNumber(int max) => new Random().Next(max);
     }
 }
